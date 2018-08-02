baf070119615e75197baa523cc7a8b3c1c969ec8
loxstomper <lochieashcroft@gmail.com>
2018-08-02 20:25:22 +1000

    first commit

---
+++ my-app/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username:string = "";
+  password:string = "";
+
+  constructor(private router:Router, private form:FormsModule) { }
+
+  ngOnInit() {
+  }
+
+  loginUser(event)
+  {
+    event.preventDefault();
+
+    if (this.username == "abc" && this.password == "123")
+    {
+        this.router.navigateByUrl('/account');
+    }
+    else
+    {
+        alert("Invalid login");
+    }
+
+  }
+
+}
