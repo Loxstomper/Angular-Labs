227045d76a800f0c4caad1d8fd7046ec1cbc079c
loxstomper <lochieashcroft@gmail.com>
2018-08-10 12:04:19 +1000

    FINISHEDgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit status!

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
   username:string = "";
   password:string = "";
 
-  constructor(private router:Router, private form:FormsModule) { }
+  constructor(private router:Router, private form:FormsModule, private http:HttpClient) { }
 
   ngOnInit() {
     console.log("Dom is ready");
@@ -25,8 +26,21 @@ export class LoginComponent implements OnInit {
   {
     event.preventDefault();
 
-    let user = {"user_id": 1, "username": this.username, "birth_date": "20/07/1969", "age": 49};
-    sessionStorage.setItem('user', JSON.stringify(user));
+    // send post request - check feedback
+    this.http.post("http://localhost:3000/login", {username:this.username, password:this.password}).subscribe(
+        res=>{
+            if (res['ok'] == 'true'){
+                let user = {"user_id": 1, "username": this.username, "birth_date": "20/07/1969", "age": 49};
+                sessionStorage.setItem('user', JSON.stringify(user));
+                this.router.navigateByUrl('/account');
+            }
+            else {
+                //this.router.navigateByUrl('/login');
+            }
+        }
+    )
+
+
 
     //localStorage.setItem("user_id", "1");
     //localStorage.setItem("user_name", this.username);
@@ -34,7 +48,7 @@ export class LoginComponent implements OnInit {
     //localStorage.setItem("user_age", "49");
 
     // no matter what login you will go the account page
-    this.router.navigateByUrl('/account');
+    // this.router.navigateByUrl('/account');
 
     /*if (this.username == "abc" && this.password == "123")
     {
